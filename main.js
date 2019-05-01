940ba23314ebc2501b5a737a9e2d911b2bd5b699
Zefwork <portnyankoanton@gmail.com>
2019-05-01 14:49:26 +0300

    string to uc

--- main.js
+++ main.js
@@ -1,35 +1,79 @@
-let budget = +prompt("Ваш бюджет?");
-let name = prompt("Название вашего магазина?");
-let time = 19;
+let budget,
+name,
+time,
+price
+
+function start() {
+  budget = +prompt("Ваш бюджет?");
+
+  while (isNaN(budget) || budget == '' || budget == null) {
+    budget = +prompt("Ваш бюджет?");
+  }
+
+  name = prompt("Название вашего магазина?").toUpperCase();
+  time = 21;
+}
+start();
 
 let mainList = {
   shopBudget: budget,
   shopName: name,
   shopGoods: [],
   employers: {},
-  open: true
+  open: true,
+  discount: false
 }
 
-for(i = 0; i < 5; i++) {
-  let a = prompt("Какой тип товаров будем продавать?");
-  if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50) {
-    console.log('All is ok!');
-    mainList.shopGoods[i] = a;
-  } else {
-
+function chooseProducts() {
+  for (i = 0; i < 5; i++) {
+    let a = prompt("Какой тип товаров будем продавать?");
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && a != '' && a.length < 50) {
+      console.log('All is ok!');
+      mainList.shopGoods[i] = a;
+    } else {
+      i = i - 1;
+    }
   }
 }
+chooseProducts();
 
-if (time < 0) {
-  console.log("It's impossible");
-} else if (time > 8 && time < 20) {
-  console.log("Time to work")
-} else if (time > 20) {
-  console.log("Time to sleep")
-} else {
-  console.log("Impossible")
+function workTime(time) {
+  if (time < 0) {
+    console.log("It's impossible");
+  } else if (time > 8 && time < 20) {
+    console.log("Time to work")
+  } else if (time < 24) {
+    console.log("Time to sleep")
+  } else {
+    console.log("Impossible")
+  }
 }
+workTime(18);
 
-alert(mainList.shopBudget / 30);
+function dayBudget() {
+  alert(mainList.shopBudget / 30);
+}
+dayBudget();
+
+function discountSystem(price) {
+  let discount = mainList.discount;
+  if(discount) {
+    price * 0.8;
+  }
+}
+discountSystem(80);
+
+function chooseEmployers() {
+  for (i = 0; i < 4; i++) {
+    let a = prompt("Имя сотрудника");
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && a != '' && a.length < 30) {
+      console.log('All is ok!');
+      mainList.employers[i] = a;
+    } else {
+      i = i - 1;
+    }
+  }
+}
+chooseEmployers();
 
 console.log(mainList);
