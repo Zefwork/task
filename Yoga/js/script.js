9642ee2c9f85d42d99988c3b28bfd5f8ac56cb3d
Unknown <portnyankoanton@gmail.com>
2019-06-13 14:18:30 +0300

    Options and AJAX loading

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function () {
+  // Tab content reveal
   let tab = document.getElementsByClassName('info-header-tab'),
       tabContent = document.getElementsByClassName('info-tabcontent'),
       info = document.getElementsByClassName('info-header')[0];
@@ -124,5 +125,84 @@ window.addEventListener('DOMContentLoaded', function () {
     overlay.style.display = 'none';
     more.classList.remove('more-splash');
     document.body.style.overflow = '';
+  })
+
+  // AJAX form sending
+  let message = new Object();
+  message.loading = "Loading...";
+  message.success = "Thank you! We will connect you soon.";
+  message.error = "Oops... Something went wrong";
+
+  let form = document.getElementsByClassName('main-form')[0],
+      mainForm = document.getElementById('form'),
+      input = form.getElementsByTagName('input'),
+      statusMessage = document.createElement('div');
+  
+  let elem = document.createElement("img");
+  elem.src = 'img/loader.gif';
+
+  statusMessage.classList.add('status');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    form.appendChild(statusMessage);
+    // AJAX
+    let req = new XMLHttpRequest();
+    req.open("POST", 'server.php');
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    let formData = new FormData(form);
+
+    req.send(formData);
+
+    req.onreadystatechange = function () {
+      if (req.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+        statusMessage.appendChild(elem);
+      } else if (req.readyState === 4) {
+        if (req.status === 200 && req.status < 300) {
+          // Adding content on page
+          statusMessage.innerHTML = message.success;
+        } else {
+          statusMessage.innerHTML = message.console.error;
+        }
+      }
+    }
+    for (let i = 0; i < input.length; i++) {
+      // Removing input text
+      input[i].value = '';
+    }
+  })
+
+  mainForm.addEventListener('submit', e => {
+    e.preventDefault();
+    mainForm.appendChild(statusMessage);
+    // AJAX
+    let req = new XMLHttpRequest();
+    req.open("POST", 'server.php');
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    let formData = new FormData(form);
+
+    req.send(formData);
+
+    req.onreadystatechange = function () {
+      if (req.readyState < 4) {
+        statusMessage.innerHTML = message.loading;
+        statusMessage.appendChild(elem);
+      } else if (req.readyState === 4) {
+        if (req.status === 200 && req.status < 300) {
+          console.log("TCL: req.onreadystatechange -> req.status", req.status)
+          // Adding content on page
+          statusMessage.innerHTML = message.success;
+        } else {
+          statusMessage.innerHTML = message.console.error;
+        }
+      }
+    }
+    for (let i = 0; i < input.length; i++) {
+      // Removing input text
+      input[i].value = '';
+    }
   })
 });
